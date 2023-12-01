5e670815dbc9fd004eaa3ad1f99a2f60b85735ea
CFC-Tech-AI <pythoncfc@gmail.com>
2023-12-01 22:08:17 +0530

    fist commit

---
+++ phrase/views.py
@@ -0,0 +1,19 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .phrase import generate_text
+from django.views.decorators.csrf import csrf_exempt
+from django import forms
+
+
+def index(request):
+    return render(request,"index.html")
+
+@csrf_exempt
+def phrasemaking(request):
+    if request.method == 'POST':        
+        prompt = request.POST.get('text', '')          
+        generated_text = generate_text(prompt)  
+        return render(request, 'phrasemaking.html', {'output': generated_text, 'input_text': prompt})
+    else:
+        return render(request, 'phrasemaking.html', {'output': '', 'input_text': ''})
