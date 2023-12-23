dd098f65321ea05b55a1998b5f2cfd0370c8898e
CFC-Tech-AI <pythoncfc@gmail.com>
2023-12-23 07:55:41 +0530

    commit

--- phrase_making/settings.py
+++ phrase_making/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6v2d65fpvm_=o8%46lbh**lst*fuo--&h(0ok&t#_4f8oixsqo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
